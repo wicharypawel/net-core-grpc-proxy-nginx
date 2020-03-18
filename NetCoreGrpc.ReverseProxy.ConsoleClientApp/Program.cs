0dd11bd0a9e5692ed879c8bd09de7941a4983dc8
Paweł Wichary <wicharypawel@gmail.com>
2020-03-18 13:12:31 +0100

    Nginx reverse proxy for gRPC

---
+++ NetCoreGrpc.ReverseProxy.ConsoleClientApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using Grpc.Net.Client;
+using NetCoreGrpc.HelloWorld.Proto;
+
+namespace NetCoreGrpc.ReverseProxy.ConsoleClientApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2)); // wait for server to wake up
+            var channelOptions = new GrpcChannelOptions() { HttpClient = CreateGrpcHttpClient(acceptSelfSignedCertificate: true) };
+            var channel = GrpcChannel.ForAddress("https://grpc-reverseproxy-nginx:443", channelOptions);
+            var client = new Greeter.GreeterClient(channel);
+            var user = "Pawel";
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+            channel.ShutdownAsync().Wait();
+        }
+
+        private static HttpClient CreateGrpcHttpClient(bool allowHttp2Unencrypted = true, bool acceptSelfSignedCertificate = false)
+        {
+            if (allowHttp2Unencrypted)
+            {
+                // https://docs.microsoft.com/en-us/aspnet/core/grpc/troubleshoot?view=aspnetcore-3.1#call-insecure-grpc-services-with-net-core-client
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            }
+            if (acceptSelfSignedCertificate)
+            {
+                var handler = new HttpClientHandler();
+                handler.ServerCertificateCustomValidationCallback = (msg, cert, chain, errors) => true;
+                var httpClient = new HttpClient(handler);
+                httpClient.Timeout = Timeout.InfiniteTimeSpan;
+                return httpClient;
+            }
+            else
+            {
+                var httpClient = new HttpClient();
+                httpClient.Timeout = Timeout.InfiniteTimeSpan;
+                return httpClient;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-net-core-grpc-nginx
+# Net Core gRPC reverse proxy Nginx
+
+# This repository
+
+Nginx can terminate, inspect, and route gRPC method calls.
+
+Main use cases for Nginx with gRPC are:
+- tls termination proxy,
+- rate limits, 
+- IP address‑based access control lists, 
+- logging,
+- publish multiple gRPC services through a single endpoint
+- load balance a cluster of gRPC services
+
+This repository shows how to configure Nginx as reverse proxy for gRPC. 
+
+```
+docker build -t grpc-reverseproxy-nginx .\NetCoreGrpc.ReverseProxy.Nginx
+```
+
+## Getting started
+
+1. Download repository 
+2. Download .Net SDK (in the moment of writing 3.1.101) & install Docker
+3. Open in VS 2019 or newer
+4. Start docker-compose in Visual Studio
+5. Navigate to containers tab and look into client container logs
+
+![Application overview](./overview.png)
+
+## Cleanup
+
+- delete all docker containers
+- delete all docker images
+  - grpc-reverseproxy-nginx:`<tag>`
+  - grpc-reverseproxy-server:`<tag>`
+  - grpc-reverseproxy-client:`<tag>`
+
+## Sources
+
+- https://www.nginx.com/blog/nginx-1-13-10-grpc/
+- https://www.nginx.com/blog/deploying-nginx-plus-as-an-api-gateway-part-3-publishing-grpc-services/
